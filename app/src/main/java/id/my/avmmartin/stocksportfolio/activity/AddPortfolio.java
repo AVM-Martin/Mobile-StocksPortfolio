43073d287479406ee131a9b9a01e7c7cee9a6a87
Mike <mikechrist21@gmail.com>
2020-05-29 21:00:15 +0700

    Home and Portfolio Activity Display Model (Version 1)

--- app/src/main/java/id/my/avmmartin/stocksportfolio/activity/AddPortfolio.java
+++ app/src/main/java/id/my/avmmartin/stocksportfolio/activity/AddPortfolio.java
@@ -1,10 +1,20 @@
 package id.my.avmmartin.stocksportfolio.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.LabelVisibilityMode;
 
 import id.my.avmmartin.stocksportfolio.R;
 
@@ -17,6 +27,36 @@ public class AddPortfolio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_portfolio);
+        ImageButton imgBtnAdd = (ImageButton) findViewById(R.id.imgBtnAdd);
+        imgBtnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(AddPortfolio.this, "Add Transaction Button Pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
+        com.google.android.material.bottomnavigation.BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_LABELED);
+        bottomNavigationView.setSelectedItemId(R.id.navPortfolio);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId() == R.id.navHome){
+                    Intent intent = new Intent(AddPortfolio.this,HomeActivity.class);
+                    startActivity(intent);
+                    return true;
+                }
+                else if(item.getItemId() == R.id.navPortfolio){
+                    Intent intent = new Intent(AddPortfolio.this,AddPortfolio.class);
+                    startActivity(intent);
+                    return true;
+                }
+                else if(item.getItemId() == R.id.navExit){
+                    finish();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     @Override
@@ -29,8 +69,8 @@ public class AddPortfolio extends AppCompatActivity {
     }
 
     private void initComponents() {
-        spBrokerID = findViewById(R.id.spBrokerID);
-        etPortfolioID = findViewById(R.id.etPortfolioID);
+        //spBrokerID = findViewById(R.id.spBrokerID);
+        //etPortfolioID = findViewById(R.id.etPortfolioID);
     }
 
     private void loadData() {
