0912952c939af9d6af6a45ca3ba4092e1843412b
AVM.Martin <avmm.martin+github@gmail.com>
2020-05-31 00:06:07 +0700

    Add factory

--- app/src/main/java/id/my/avmmartin/stocksportfolio/data/DataFactory.java
+++ app/src/main/java/id/my/avmmartin/stocksportfolio/data/DataFactory.java
@@ -2,15 +2,43 @@ package id.my.avmmartin.stocksportfolio.data;
 
 import android.content.Context;
 
-public class DataFactory {
+import java.util.Calendar;
+
+import id.my.avmmartin.stocksportfolio.data.model.Portfolio;
+import id.my.avmmartin.stocksportfolio.data.model.Transaction;
+
+public class DataFactory extends DataManager {
     public static void generate(Context context) {
         new BrokerFactory(context);
         new StockFactory(context);
+
+        DataFactory instance = new DataFactory(context);
+        instance.generatePortfolio();
+        instance.generateTransaction();
+    }
+
+    // private generator
+
+    private void generatePortfolio() {
+        if (portfolioSize() == 0) {
+            insertPortfolio(new Portfolio("LG", "Trima AVM", Calendar.getInstance()));
+            insertPortfolio(new Portfolio("LG", "Trima AVM 2", Calendar.getInstance()));
+        }
+    }
+
+    private void generateTransaction() {
+        if (transactionSize() == 0) {
+            for (int i = 0; i < 100; i++) {
+                insertTransaction(new Transaction(
+                    1, "BTPS", 1, Calendar.getInstance(), i, i, i
+                ));
+            }
+        }
     }
 
     // constructor
 
-    private DataFactory() {
-        // none
+    private DataFactory(Context context) {
+        super(context);
     }
 }
