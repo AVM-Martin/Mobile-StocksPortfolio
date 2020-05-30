85da87fd65e6499ac1c9473ec362882417234e87
AVM.Martin <avmm.martin+github@gmail.com>
2020-05-30 16:42:45 +0700

    Implement all methods inside data manager

--- app/src/main/java/id/my/avmmartin/stocksportfolio/data/DataManager.java
+++ app/src/main/java/id/my/avmmartin/stocksportfolio/data/DataManager.java
@@ -3,6 +3,9 @@ package id.my.avmmartin.stocksportfolio.data;
 import android.content.Context;
 
 import id.my.avmmartin.stocksportfolio.data.model.Broker;
+import id.my.avmmartin.stocksportfolio.data.model.Portfolio;
+import id.my.avmmartin.stocksportfolio.data.model.Stock;
+import id.my.avmmartin.stocksportfolio.data.model.Transaction;
 
 public class DataManager {
     // broker
@@ -19,11 +22,83 @@ public class DataManager {
         return brokerManager.getByPosition(position);
     }
 
+    // portfolio
+
+    public int portfolioSize() {
+        return portfolioManager.size();
+    }
+
+    public void insertPortfolio(Portfolio portfolio) {
+        portfolioManager.insert(portfolio);
+    }
+
+    public Portfolio getPortfolioById(int id) {
+        return portfolioManager.getById(id);
+    }
+
+    public Portfolio getPortfolioByPosition(int position) {
+        return portfolioManager.getByPosition(position);
+    }
+
+    public void updatePortfolio(Portfolio portfolio) {
+        portfolioManager.update(portfolio);
+    }
+
+    // stock
+
+    public int stockSize() {
+        return brokerManager.size();
+    }
+
+    public Stock getStockById(String id) {
+        return stockManager.getById(id);
+    }
+
+    public Stock getStockByPosition(int position) {
+        return stockManager.getByPosition(position);
+    }
+
+    // transaction
+
+    public int transactionSize() {
+        return transactionManager.size();
+    }
+
+    public int transactionSizeByPortfolio(int portfolioId) {
+        return transactionManager.sizeByPortfolio(portfolioId);
+    }
+
+    public void insert(Transaction transaction) {
+        transactionManager.insert(transaction);
+    }
+
+    public Transaction getById(int id) {
+        return transactionManager.getById(id);
+    }
+
+    public Transaction getByPosition(int position) {
+        return transactionManager.getByPosition(position);
+    }
+
+    public Transaction getByPortfolioByPosition(int portfolioId, int position) {
+        return transactionManager.getByPortfolioByPosition(portfolioId, position);
+    }
+
+    public void update(Transaction transaction) {
+        transactionManager.update(transaction);
+    }
+
     // constructor
 
     private BrokerManager brokerManager;
+    private PortfolioManager portfolioManager;
+    private StockManager stockManager;
+    private TransactionManager transactionManager;
 
     public DataManager(Context context) {
         brokerManager = new BrokerManager(context);
+        portfolioManager = new PortfolioManager(context);
+        stockManager = new StockManager(context);
+        transactionManager = new TransactionManager(context);
     }
 }
