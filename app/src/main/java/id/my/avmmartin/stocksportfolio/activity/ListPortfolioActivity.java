996146671cf4aa4a3de7bbea56030b3e8f535c8b
AVM.Martin <avmm.martin+github@gmail.com>
2020-05-30 23:30:14 +0700

    Add list portfolio activity

---
+++ app/src/main/java/id/my/avmmartin/stocksportfolio/activity/ListPortfolioActivity.java
@@ -0,0 +1,69 @@
+package id.my.avmmartin.stocksportfolio.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Spinner;
+
+import id.my.avmmartin.stocksportfolio.R;
+import id.my.avmmartin.stocksportfolio.StocksPortfolio;
+import id.my.avmmartin.stocksportfolio.activity.components.PortfolioSpinnerAdapter;
+import id.my.avmmartin.stocksportfolio.activity.components.TransactionListAdapter;
+import id.my.avmmartin.stocksportfolio.data.DataFactory;
+
+public class ListPortfolioActivity extends AppCompatActivity {
+    private StocksPortfolio mainApp;
+
+    private Spinner spPortfolio;
+    private RecyclerView rvTransaction;
+
+    private PortfolioSpinnerAdapter portfolioSpinnerAdapter;
+    private TransactionListAdapter transactionListAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_portofolio);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        DataFactory.generate(this);
+        initComponents();
+        loadData();
+        setEvents();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        portfolioSpinnerAdapter.notifyDataSetChanged();
+        transactionListAdapter.notifyDataSetChanged();
+    }
+
+    private void initComponents() {
+        mainApp = (StocksPortfolio) getApplication();
+
+        spPortfolio = findViewById(R.id.spPortfolio);
+        rvTransaction = findViewById(R.id.rvTransactions);
+
+        portfolioSpinnerAdapter = new PortfolioSpinnerAdapter(mainApp, this);
+        transactionListAdapter = new TransactionListAdapter(mainApp, this);
+    }
+
+    private void loadData() {
+        spPortfolio.setAdapter(portfolioSpinnerAdapter);
+
+        rvTransaction.setLayoutManager(new LinearLayoutManager(this));
+        rvTransaction.setAdapter(transactionListAdapter);
+    }
+
+    private void setEvents() {
+        // none
+    }
+}
