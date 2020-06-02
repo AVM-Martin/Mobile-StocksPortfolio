88503a689437352992fe363c85c11a12690370bf
AVM.Martin <avmm.martin+github@gmail.com>
2020-06-02 10:02:25 +0700

    Add stock price data manager

---
+++ app/src/main/java/id/my/avmmartin/stocksportfolio/data/manager/StockPriceManager.java
@@ -0,0 +1,84 @@
+package id.my.avmmartin.stocksportfolio.data.manager;
+
+import android.util.Log;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+import id.my.avmmartin.stocksportfolio.utils.Constants;
+import id.my.avmmartin.stocksportfolio.utils.OnlineDataLoaderUtils;
+
+public class StockPriceManager extends HashMap<String, Integer> {
+    private static final String URL = Constants.JSON_URL_STOCK_PRICE;
+
+    public void reloadOnlineData(final OnlineDataLoaderUtils loaderUtils, String stockId) {
+        loaderUtils.onPreExecute();
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+            String.format(URL, stockId),
+            null,
+            new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        JSONArray prices = response
+                            .getJSONObject("chart")
+                            .getJSONArray("result")
+                            .getJSONObject(0)
+                            .getJSONObject("indicators")
+                            .getJSONArray("quote")
+                            .getJSONObject(0)
+                            .getJSONArray("close");
+
+                        for (int i = 0; i < prices.length(); i++) {
+                            try {
+                                int price = prices.getInt(i);
+                                // put(stockId, price);
+                                Log.d("hoho", Integer.toString(price));
+                            } catch (Exception e) {
+                                Log.e("hoho", "from stock price manager", e);
+                            }
+                        }
+
+                    } catch (JSONException e) {
+                        //
+                    } finally {
+                        loaderUtils.onSuccessExecute();
+                    }
+
+                    loaderUtils.onPostExecute();
+                }
+            },
+            new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError e) {
+                    loaderUtils.onErrorExecute();
+                }
+            }
+        );
+
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    public Integer getPrice(String stockId) {
+        return get(stockId);
+    }
+
+    // constructor
+
+    private RequestQueue requestQueue;
+
+    public StockPriceManager(RequestQueue requestQueue) {
+        this.requestQueue = requestQueue;
+
+        clear();
+    }
+}
--- app/src/main/java/id/my/avmmartin/stocksportfolio/utils/Constants.java
+++ app/src/main/java/id/my/avmmartin/stocksportfolio/utils/Constants.java
@@ -5,6 +5,9 @@ public class Constants {
 
     public static final String DATE_FORMAT ="dd/MM/yyyy";
 
+    public static final String JSON_URL_STOCK_PRICE =
+        "https://query1.finance.yahoo.com/v8/finance/chart/%s.JK?range=1d&interval=1m&includePrePost=false&indicators=close";
+
     // constructor
 
     private Constants() {
